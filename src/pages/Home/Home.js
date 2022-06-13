df7ed7763bab91db97f1f60e89048f0411f03b2f
Pawan Bhatta <pawanbhatta00@gmail.com>
2022-06-13 13:51:52 +0545

    reusable components added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { Navbar, Sidebar } from "../../components";
+import { Navbar, Sidebar, Widget } from "../../components";
 import "./home.scss";
 
 const Home = () => {
@@ -7,7 +7,12 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        home container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
